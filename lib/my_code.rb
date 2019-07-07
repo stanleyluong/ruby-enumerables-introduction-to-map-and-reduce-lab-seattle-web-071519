39b84772168bd41be25ef562f508d65a32c9fe4f
Stanley Luong <slluong@uw.edu>
2019-07-07 06:04:06 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -70,5 +70,15 @@ def reduce_to_all_true(array)
   return true  
 end
 
-def reduce_to_any_true 
+def reduce_to_any_true(array)
+  i = 0 
+  while i < array.length do 
+    if array[i] == true
+      return true
+    else 
+      i+=1 
+    end
+  end
+  return false
+end 
     
